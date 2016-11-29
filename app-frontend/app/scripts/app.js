fb4cdfc4f14177bbc19cd1b604482e4e4dbd5e19
Jan Vega <jan.vega1@upr.edu>
2016-11-28 22:19:22 -0400

    add home state with controller and initial data

--- app-frontend/app/scripts/app.js
+++ app-frontend/app/scripts/app.js
@@ -19,7 +19,47 @@ angular
         url: '/',
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .state('home', {
+        url: '/home',
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
       });
     $urlRouterProvider.otherwise('/');
-  });
+  })
+  .factory('data', ['$http', function($http){
+    var data = {};
+    data.user = {
+      person_id: 1,
+      person_name: 'Manuel',
+      person_last_name: 'Rodriguez',
+      person_type: 0,
+      person_uprm_id: '802864000',
+      person_rfid_id: 'changeme',
+    };
+    data.sections = [
+      {
+        section_id: 1,
+        section_name: '020',
+        section_term: 1,
+        course: {
+          course_id: 1,
+          course_code: 'ICOM5007',
+          course_name: 'Operative Systems',
+        },
+      },
+      {
+        section_id: 2,
+        section_name: '030',
+        section_term: 1,
+        course: {
+          course_id: 2,
+          course_code: 'ICOM5016',
+          course_name: 'Database Systems',
+        },
+      }
+    ];
+    return data;
+  }]);
 
