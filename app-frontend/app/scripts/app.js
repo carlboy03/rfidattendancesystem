ef03f5520a5f299539019d35f639354db2c41e40
Jan Vega <jan.vega1@upr.edu>
2016-12-01 19:52:35 -0400

    data entry for Angular factory

--- app-frontend/app/scripts/app.js
+++ app-frontend/app/scripts/app.js
@@ -109,6 +109,126 @@ angular
           person_rfid_id: 'changeme'
         }
       },
+      {
+        attendance_id: 3,
+        attendance_status: false,
+        person: {
+          person_id: 4,
+          person_name: 'Jose',
+          person_last_name: 'Lugo',
+          person_type: 1,
+          person_uprm_id: '802091535',
+          person_rfid_id: 'changeme'
+        }
+      },
+      {
+        attendance_id: 4,
+        attendance_status: true,
+        person: {
+          person_id: 5,
+          person_name: 'Carlos',
+          person_last_name: 'Bernal',
+          person_type: 1,
+          person_uprm_id: '802071244',
+          person_rfid_id: 'changeme'
+        }
+      },
+      {
+        attendance_id: 5,
+        attendance_status: true,
+        person: {
+          person_id: 6,
+          person_name: 'Maria',
+          person_last_name: 'Jimenez',
+          person_type: 1,
+          person_uprm_id: '802084234',
+          person_rfid_id: 'changeme'
+        }
+      },
+      {
+        attendance_id: 6,
+        attendance_status: false,
+        person: {
+          person_id: 7,
+          person_name: 'Michael',
+          person_last_name: 'Mercado',
+          person_type: 1,
+          person_uprm_id: '844091435',
+          person_rfid_id: 'changeme'
+        }
+      },
+      {
+        attendance_id: 7,
+        attendance_status: true,
+        person: {
+          person_id: 8,
+          person_name: 'Joe',
+          person_last_name: 'Lopez',
+          person_type: 1,
+          person_uprm_id: '802091234',
+          person_rfid_id: 'changeme'
+        }
+      },
+      {
+        attendance_id: 8,
+        attendance_status: true,
+        person: {
+          person_id: 9,
+          person_name: 'Edgar',
+          person_last_name: 'Fuentes',
+          person_type: 1,
+          person_uprm_id: '802081234',
+          person_rfid_id: 'changeme'
+        }
+      },
+      {
+        attendance_id: 9,
+        attendance_status: false,
+        person: {
+          person_id: 10,
+          person_name: 'Jose',
+          person_last_name: 'Rivera',
+          person_type: 1,
+          person_uprm_id: '802091535',
+          person_rfid_id: 'changeme'
+        }
+      },
+      {
+        attendance_id: 10,
+        attendance_status: true,
+        person: {
+          person_id: 11,
+          person_name: 'Ricky',
+          person_last_name: 'Soto',
+          person_type: 1,
+          person_uprm_id: '802071244',
+          person_rfid_id: 'changeme'
+        }
+      },
+      {
+        attendance_id: 11,
+        attendance_status: true,
+        person: {
+          person_id: 12,
+          person_name: 'Lola',
+          person_last_name: 'Velez',
+          person_type: 1,
+          person_uprm_id: '801084534',
+          person_rfid_id: 'changeme'
+        }
+      },
+      {
+        attendance_id: 12,
+        attendance_status: false,
+        person: {
+          person_id: 13,
+          person_name: 'Lolo',
+          person_last_name: 'Lugo',
+          person_type: 1,
+          person_uprm_id: '844093456',
+          person_rfid_id: 'changeme'
+        }
+      }
     ];
     return data;
   }]);
