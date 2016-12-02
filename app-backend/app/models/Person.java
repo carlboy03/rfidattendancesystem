9a315a1bd6b657ded57d7caa87bf3a9fe5ed42a0
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-12-02 06:43:58 +0000

    Updated Person Model
    Added email/password fields

--- app-backend/app/models/Person.java
+++ app-backend/app/models/Person.java
@@ -12,31 +12,39 @@ public class Person extends Model{
     public String   person_name;
     public String   person_last_name;
     public int  person_type;
-    public String   pass;
+    public String   person_uprm_id;
     public String   rfid;
+    public String person_email;
+    public String person_password;
 
     public Person(){
-        int   person_id;
+        int   person_id=-1;
         String   person_name;
-         String   person_last_name;
-         int  person_type;
-         String   pass;
-         String   rfid;
+        String   person_last_name;
+        int  person_type;
+        String   person_uprm_id;
+        String   rfid;
+        String person_email;
+        String person_password;
     }
 
-    public Person ( int      person_id,
-             String   person_name,
-             String   person_last_name,
-                    int  person_type,
-             String   pass,
-             String   rfid)
+    public Person (
+            int      person_id,
+            String   person_name,
+            String   person_last_name,
+            int  person_type,
+            String   pass,
+            String   rfid,
+            String person_email, String person_password)
     {
         this.person_id=person_id;
         this.person_name=person_name;
         this.person_last_name=person_last_name;
         this.person_type=person_type;
-        this.pass=pass;
+        this.person_uprm_id=pass;
         this.rfid=rfid;
+        this.person_email=person_email;
+        this.person_password=person_password;
     }
 
     public int getPerson_id() {
@@ -71,12 +79,12 @@ public class Person extends Model{
         this.person_type = person_type;
     }
 
-    public String getPass() {
-        return pass;
+    public String getPerson_uprm_id() {
+        return person_uprm_id;
     }
 
-    public void setPass(String pass) {
-        this.pass = pass;
+    public void setPerson_uprm_id(String person_uprm_id) {
+        this.person_uprm_id = person_uprm_id;
     }
 
     public String getRfid() {
@@ -85,6 +93,22 @@ public class Person extends Model{
 
     public void setRfid(String rfid) {
         this.rfid = rfid;
+    }
+
+    public String getPerson_email() {
+        return person_email;
+    }
+
+    public void setPerson_email(String person_email) {
+        this.person_email = person_email;
+    }
+
+    public String getPerson_password() {
+        return person_password;
+    }
+
+    public void setPerson_password(String person_password) {
+        this.person_password = person_password;
     }
 }
 
