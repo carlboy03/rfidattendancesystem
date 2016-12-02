5c7ccc7fd5823daafc4f2d04aed244c4f14f4c71
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-12-02 05:04:45 +0000

    Fixed SQL for Attendance
    Session is not referenced in section

--- app-backend/conf/evolutions/default/3.sql
+++ app-backend/conf/evolutions/default/3.sql
@@ -12,10 +12,11 @@ CREATE TABLE attendance (
   attendance_status BOOLEAN NOT NULL DEFAULT FALSE,
   attendance_date TIMESTAMP WITH TIME ZONE DEFAULT now(),
   attendance_student_id BIGINT NOT NULL REFERENCES person(person_id), -- Student ID
-  attendance_section_session BIGINT NOT NULL REFERENCES sections(section_session), -- The nth session of a x credit course.
+  attendance_section_session BIGINT NOT NULL, -- The nth session of a x credit course.
   attendance_comment TEXT,
   attendance_section_id BIGINT NOT NULL
 );
+
 
 # --- !Downs
 
