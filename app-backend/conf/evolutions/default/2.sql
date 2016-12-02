f7134bb5b400eaace5d0526ed9e332dfd0e36f6d
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-12-02 04:46:00 +0000

    Added Data to  Courses Table

--- app-backend/conf/evolutions/default/2.sql
+++ app-backend/conf/evolutions/default/2.sql
@@ -8,6 +8,28 @@ CREATE TABLE course (
   course_name VARCHAR(100) NOT NULL
 );
 
+INSERT INTO course (course_code, course_name)
+  VALUES( 'MATE3005', 'PRE CALCULO'),
+    ('QUIM3001', 'Quimica General I'),
+    ('INGE3011', 'Graficas de Ingenieria'),
+    ('ESPA3101', 'Curso Basico de Espanol'),
+    ('EDFI3245', 'Natacion Elemental'),
+    ('INGL3101', 'Curso Basico Ingles'),
+    ('QUIM3132', 'Quimica General II'),
+    ('INGL3102', 'Curso Basico Ingles'),
+    ('ESPA3102', 'Curso Basico de Espanol'),
+    ('EDFI3596', 'Fundamentos de Balonpie'),
+    ('MATE3152', 'Calculo II'),
+    ('ICOM4215', 'Arquitectura y Organizacion'),
+    ('INEL4301', 'Teoria de Comunicacion'),
+    ('INEL4225', 'Lab Electronica Digital'),
+    ('INME4045', 'Termodinamica General Para Ingenieros'),
+    ('HIST3222', 'Historia de Las Antillas'),
+    ('ICOM5007', 'Programacion de Sistemas Operativos'),
+    ('ININ4015', 'Analisis Economico para Ingenieros'),
+    ('CISO3121', 'Intro a Ciencias Sociales'),
+    ('ICOM4217', 'Diseno de sistemas embebidos');
+
 CREATE TABLE sections (
   section_id SERIAL NOT NULL PRIMARY KEY,
   section_name VARCHAR(255) NOT NULL,
