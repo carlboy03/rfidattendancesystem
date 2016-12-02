b2f4c77d51e2a9c7370a54c98ad3b56697b0e13c
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-12-02 04:29:14 +0000

    Added Data to Person Table
    Data to Person Table

--- app-backend/conf/evolutions/default/1.sql
+++ app-backend/conf/evolutions/default/1.sql
@@ -15,7 +15,16 @@ CREATE TABLE person (
 INSERT INTO person(person_name, person_last_name, person_type, person_uprm_id, person_rfid, person_email, person_password)
     VALUES ('Manuel', 'Jimenez', 1, '802669090', 'cdjkiuelow90375jckdl', 'manuel.jimenez@upr.edu', 'password'),
       ('Jan', 'Vega', 0, '802119390', 'jkldoeu73892mch3849k', 'jan.vega1@upr.edu', 'password'),
-      ('Carlos', 'Fuentes', 0, '801082118', 'jkdoe740pkstekdu271h', 'carlos.fuentes1@upr.edu', 'password');
+      ('Carlos', 'Fuentes', 0, '801082118', 'jkdoe740pkstekdu271h', 'carlos.fuentes1@upr.edu', 'password'),
+      ('Maria', 'Jimenez', 0, '840103836', 'jkdoe740pkstekdu271i', 'maria.jimenez21@upr.edu', 'password'),
+      ('Michael', 'Mercado', 0, '844104968', 'jkdoe740pkstekdu271j', 'michael.mercado5@upr.edu', 'password'),
+      ('Jaime', 'Gonzalez', 0, '801081111', 'jkdoe740pkstekdu271l', 'jaime.gonzalez5@upr.edu', 'password'),
+      ('Charlie', 'Castro', 0, '802101111', 'jkdoe740pkstekdu271n', 'charlie.castro@upr.edu', 'password'),
+      ('Jean', 'Jordan', 0, '802101114', 'jkdoe740pkstekdu271o', 'jean.yordan@upr.edu', 'password'),
+      ('Jean', 'Rodriguez', 0, '802101115', 'jkdoe740pkstekdu271p', 'jean.rodriguez@upr.edu', 'password'),
+      ('Armando', 'Ortiz', 0, '802101116', 'jkdoe740pkstekdu271q', 'armando.ortiz1@upr.edu', 'password'),
+      ('Javier', 'Colon', 0, '802101117', 'jkdoe740pkstekdu271r', 'javier.colon15@upr.edu', 'password'),
+      ('Carlos', 'Bernal', 1, '802141234', 'cdjkiuelow90375jckdm', 'carlosjulio.bernal@upr.edu', 'password');
 
 # --- !Downs
 
