16264f9c7c2d8319cfedc2c67d8c262e796d279d
Jan Vega <jan.vega1@upr.edu>
2016-12-08 21:46:32 -0400

    implement home and section states with their factories - preload objects with app state - save app state in localStorage

--- app-frontend/app/scripts/app.js
+++ app-frontend/app/scripts/app.js
@@ -21,14 +21,24 @@ angular
         controller: 'MainCtrl'
       })
       .state('home', {
-        url: '/home',
+        url: '/home/:id',
         templateUrl: 'views/home.html',
-        controller: 'HomeCtrl'
+        controller: 'HomeCtrl',
+        resolve: {
+          sectionPromise: ['data', '$stateParams', function(data, $stateParams){
+            return data.getSections($stateParams.id)
+          }]
+        }
       })
       .state('section', {
         url: '/section/:id',
         templateUrl: 'views/section.html',
         controller: 'SectionCtrl',
+        resolve: {
+          attendancePromise: ['data', '$stateParams', function(data, $stateParams){
+            return data.getSection($stateParams.id);
+          }]
+        }
       })
       .state('about', {
         url: '/about',
@@ -36,207 +46,73 @@ angular
       });
     $urlRouterProvider.otherwise('/');
   })
-  .factory('data', ['$http', function(){
+  .factory('data', ['$http', function($http){
     var data = {};
-    data.setCurrentSection = function(section){
-      data.currentSection = section;
+    // State methods
+    data.selectCourse = function(course){
+      localStorage.setObject('course_code', course.course_code);
     };
-    data.hasSelectedSection = function(){
-      if(data.currentSection === null) {
+    data.selectedCourse = function(){
+      return localStorage.getObject('course_code');
+    };
+    data.selectSection = function(section){
+      localStorage.setObject('section_name', section.section_name);
+    };
+    data.selectedSection = function(){
+      return localStorage.getObject('section_name');
+    };
+    // Home View
+    data.getSections = function(id){
+      return $http.get('/app-backend/section/'+id).success(function(sections){
+        data.sections = sections;
+      });
+    };
+    data.hasCurrentSections = function(){
+      if(!data.sections){
         return false;
       }
       else {
         return true;
       }
     };
-    data.isLoggedIn = function(){
-      if(data.user === null){
+    data.currentSections = function(){
+      if(data.hasCurrentSections()){
+        return data.sections;
+      }
+      return null;
+    };
+    // Attendance View
+    data.getSection = function(id){
+      return $http.get('/app-backend/attendance/'+id).success(function(section){
+        data.section = section;
+      });
+    };
+    data.hasCurrentSection = function(){
+      if(!data.section){
         return false;
       }
       else {
         return true;
       }
     };
-    data.sections = [
-      {
-        section_id: 1,
-        section_name: '020',
-        section_term: 1,
-        course: {
-          course_id: 1,
-          course_code: 'ICOM5007',
-          course_name: 'Operative Systems',
-        },
-      },
-      {
-        section_id: 2,
-        section_name: '030',
-        section_term: 1,
-        course: {
-          course_id: 2,
-          course_code: 'ICOM5016',
-          course_name: 'Database Systems',
-        },
+    data.currentSection = function(){
+      if(data.hasCurrentSection()){
+        return data.section;
       }
-    ];
-    data.records = [
-      {
-        attendance_id: 1,
-        attendance_status: true,
-        person: {
-          person_id: 2,
-          person_name: 'Jan',
-          person_last_name: 'Vega',
-          person_type: 1,
-          person_uprm_id: '802091234',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 2,
-        attendance_status: true,
-        person: {
-          person_id: 3,
-          person_name: 'Carlos',
-          person_last_name: 'Fuentes',
-          person_type: 1,
-          person_uprm_id: '802081234',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 3,
-        attendance_status: false,
-        person: {
-          person_id: 4,
-          person_name: 'Jose',
-          person_last_name: 'Lugo',
-          person_type: 1,
-          person_uprm_id: '802091535',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 4,
-        attendance_status: true,
-        person: {
-          person_id: 5,
-          person_name: 'Carlos',
-          person_last_name: 'Bernal',
-          person_type: 1,
-          person_uprm_id: '802071244',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 5,
-        attendance_status: true,
-        person: {
-          person_id: 6,
-          person_name: 'Maria',
-          person_last_name: 'Jimenez',
-          person_type: 1,
-          person_uprm_id: '802084234',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 6,
-        attendance_status: false,
-        person: {
-          person_id: 7,
-          person_name: 'Michael',
-          person_last_name: 'Mercado',
-          person_type: 1,
-          person_uprm_id: '844091435',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 7,
-        attendance_status: true,
-        person: {
-          person_id: 8,
-          person_name: 'Joe',
-          person_last_name: 'Lopez',
-          person_type: 1,
-          person_uprm_id: '802091234',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 8,
-        attendance_status: true,
-        person: {
-          person_id: 9,
-          person_name: 'Edgar',
-          person_last_name: 'Fuentes',
-          person_type: 1,
-          person_uprm_id: '802081234',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 9,
-        attendance_status: false,
-        person: {
-          person_id: 10,
-          person_name: 'Jose',
-          person_last_name: 'Rivera',
-          person_type: 1,
-          person_uprm_id: '802091535',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 10,
-        attendance_status: true,
-        person: {
-          person_id: 11,
-          person_name: 'Ricky',
-          person_last_name: 'Soto',
-          person_type: 1,
-          person_uprm_id: '802071244',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 11,
-        attendance_status: true,
-        person: {
-          person_id: 12,
-          person_name: 'Lola',
-          person_last_name: 'Velez',
-          person_type: 1,
-          person_uprm_id: '801084534',
-          person_rfid_id: 'changeme'
-        }
-      },
-      {
-        attendance_id: 12,
-        attendance_status: false,
-        person: {
-          person_id: 13,
-          person_name: 'Lolo',
-          person_last_name: 'Lugo',
-          person_type: 1,
-          person_uprm_id: '844093456',
-          person_rfid_id: 'changeme'
-        }
-      }
-    ];
+      return null;
+    };
     return data;
   }])
   .factory('auth', ['$http', function($http){
     var auth = {};
     auth.currentUser = function(){
       if(auth.isLoggedIn()){
-        return auth.user;
+        return localStorage.getObject('user');
       }
       return null;
     };
     auth.isLoggedIn = function(){
-      if(!auth.user){
+      if(!localStorage.getObject('user')){
         return false;
       }
       else {
@@ -246,7 +122,17 @@ angular
     auth.login = function(user){
       return $http.post('/app-backend/login', user).success(function(data){
         auth.user = data;
+        localStorage.setObject('user', data);
       });
     };
     return auth;
   }]);
+
+Storage.prototype.setObject = function(key, value) {
+  this.setItem(key, JSON.stringify(value));
+};
+
+Storage.prototype.getObject = function(key) {
+  var value = this.getItem(key);
+  return value && JSON.parse(value);
+};
